edb63705479ec23b38c96b7f101e8610f27d7393
marcos <ferreira.marcos.1988>
2021-10-26 17:42:39 -0300

    cambios en el footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -9,58 +9,69 @@ export default function footer(){
             <footer className="text-center text-white" style={{backgroundColor: "grey"}}>
             
             <div className="container pt-4">
-                
+                <div className="row">
+                    <div className="col">
+                <h6>Marcos Ferreira</h6>
                 <section className="mb-4">                
                     <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://es-la.facebook.com/"
+                        href="https://www.facebook.com/marcos.d.ferreira.73"
+                        target="_blank"
                         role="button"
                         data-mdb-ripple-color="red">
                         <i className="fab fa-facebook-f"></i>
                     </a>
-                    
+                                       
                     <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://twitter.com/?lang=es"
-                        role="button"
-                        data-mdb-ripple-color="white">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-
-                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://www.google.com/"
-                        role="button"
-                        data-mdb-ripple-color="dark">
-                        <i className="fab fa-google"></i>
-                    </a>
-                    
-                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://www.instagram.com/?hl=es"
-                        role="button"
-                        data-mdb-ripple-color="">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                    
-                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://ar.linkedin.com/"
+                        href="#!"
                         role="button"
                         data-mdb-ripple-color="dark"> 
                         <i className="fab fa-linkedin"></i>
                     </a>
                     
                     <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
-                        href="https://github.com/mmduran77/bootCamp-AppMovie/"
+                        href="#!"
                         role="button"
                         data-mdb-ripple-color="dark">
                         <i className="fab fa-github"></i>
                     </a>
+                    
                 </section>
+                </div>
+                <div className="col">
+                <h6>Mauro Duran</h6>
+                <section className="mb-4">                
+                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="https://www.facebook.com/marcos.d.ferreira.73"
+                        target="_blank"
+                        role="button"
+                        data-mdb-ripple-color="red">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>   
+                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark"> 
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    
+                    <a  className="btn btn-link btn-floating btn-lg text-dark m-1"
+                        href="#!"
+                        role="button"
+                        data-mdb-ripple-color="dark">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    
+                </section>
+                </div>
+                </div>
                 
             </div>
             
 
             
             <div className="text-center text-dark p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                © 2021 Copyright 
-                <a className="text-dark" href="https://mdbootstrap.com/">FullPelis</a>
+                © 2021 Copyright   
+                <a className="text-dark" href="https://mdbootstrap.com/">  FullPelis</a>
             </div>
             
             </footer>
