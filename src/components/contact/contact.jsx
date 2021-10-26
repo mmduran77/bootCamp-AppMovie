c5d7e36689ea57440d8948a53b7431e7bbf3dd0c
Miguel Mauro Duran <mmduran77@gmail.com>
2021-10-26 17:34:10 -0300

    Correccion errores en about, home y contact

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,12 +6,15 @@ import Container from "react-bootstrap/Container"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import { useState } from "react"
+import { useHistory } from "react-router";
 import emailjs from "emailjs-com"
 import "../../assets/css/styles.min.css"
 import "../../assets/bootstrap/css/bootstrap.min.css"
 import "../../assets/css/Contact-Form-Clean.css"
+import Footer1 from "../Footer/footer1";
 
 
+// CONTROL DE VALIDACIONES DEL FORMULARIO
 export function Validacion(input){
     let errors = {}
     if(!input.email){
@@ -36,6 +39,12 @@ export function Validacion(input){
 
 export default function Contact() {
     
+    const history = useHistory();
+    
+    const redireccionamiento = () => {
+        history.push("/home")
+    }; 
+
     const [state, setState] = useState({
         email: '',
         name: '',
@@ -48,6 +57,7 @@ export default function Contact() {
         message:"El Mensaje es requerido"
         })  
 
+    //METODO QUE ENVIA A TRAVES DE EMAILJS UN MENSAJE A UN CORREO ELECTRONICO
     function onSubmitForm(e) {
         e.preventDefault();
         emailjs.sendForm('service_akbwd8o', 'template_k4riam9', e.target, 'user_3KLQnK8X1eJKjg1WLGiAj')
@@ -58,7 +68,7 @@ export default function Contact() {
           });
         e.target.reset()
         alert("Mensaje enviado");
-        
+        redireccionamiento();
     }
 
     function onHandleChange(e) {
@@ -76,25 +86,6 @@ export default function Contact() {
     return( 
         <div>
             
-                {/* <nav className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
-                    <div className="container-fluid d-flex flex-column p-0">
-                        <a className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="/">
-                        <div className="sidebar-brand-icon rotate-n-15"><i className="fas fa-laugh-wink"></i></div>
-                        <div className="sidebar-brand-text mx-3"><span>Fullpelis</span></div>
-                        </a>
-                        <hr className="sidebar-divider my-0"/>
-                        <ul className="navbar-nav text-light" id="accordionSidebar">
-                            <li className="nav-item"><a className="nav-link" href="/about"><i className="fas fa-user"></i><span>About</span></a></li>
-
-                            <li className="nav-item"><a className="nav-link" href="register.html"><i className="fas fa-user-circle"></i><span>Register</span></a></li>
-
-                            <li className="nav-item"><a className="nav-link active" href="/notfound"><i className="fas fa-exclamation-circle"></i><span>Page Not Found</span></a></li>
-
-                            <li className="nav-item"><a className="nav-link" href="/contact"><i className="fas fa-window-maximize"></i><span>Contact</span></a></li>
-                        </ul>
-                        <div className="text-center d-none d-md-inline"><button className="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
-                    </div>
-                </nav> */}
             <section className="contact-clean">
                     <form onSubmit={(e)=>onSubmitForm(e)}>
                         <h2 className="text-center">Contáctanos</h2>
@@ -108,20 +99,21 @@ export default function Contact() {
                                 {fails.email ? <p> {fails.email} </p> : null}
                             </small>
                         </div>
+
                         <div className="mb-3">
                             <textarea className="form-control" name="message" onChange={(e) => onHandleChange(e)} placeholder="Mensaje" rows="14"></textarea>
                             <small className="form-text text-danger">
                                 {fails.message ? <p> {fails.message} </p> : null}
-                            </small>
-                           
+                            </small>                           
                         </div>
+
                         <div className="d-xl-flex d-flex justify-content-around">
                             <button className="btn btn-primary" type="submit" disabled={ fails.email || fails.name ||fails.message ? true : false }>enviar</button>
-                            <button className="btn btn-primary" type="submit">cancelar</button>
-                            
+                            <button onClick={redireccionamiento} className="btn btn-primary" type="submit">cancelar</button>
                         </div>
                     </form>
-                </section>            
+                </section>          
+                <Footer1/>  
         </div>
     )
 }
